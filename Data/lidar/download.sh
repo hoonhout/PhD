a73483eb40293ec2549c33e15363a14cbb0b184f
Bas Hoonhout <bas@hoonhout.com>
2016-08-11 15:22:03 +0200

    Changed script order, added data and resulting figures

---
+++ Data/lidar/download.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+wget http://www.rijkswaterstaat.nl/apps/geoservices/geodata/dmc/kust2_2011_2/kust2_2011_2.zip
+wget http://www.rijkswaterstaat.nl/apps/geoservices/geodata/dmc/kust2_2012_2/kust2_2012_2.zip
+wget http://www.rijkswaterstaat.nl/apps/geoservices/geodata/dmc/kust2_2013_2/kust2_2013_2.zip
+wget http://www.rijkswaterstaat.nl/apps/geoservices/geodata/dmc/kust2_2014_2/kust2_2014_2.zip
+wget http://www.rijkswaterstaat.nl/apps/geoservices/geodata/dmc/kust2_2015_2/kust2_2015_2.zip
+wget http://www.rijkswaterstaat.nl/apps/geoservices/geodata/dmc/kust_2011_5/kust_2011_5.zip
+wget http://www.rijkswaterstaat.nl/apps/geoservices/geodata/dmc/kust_2012_5/kust_2012_5.zip
+wget http://www.rijkswaterstaat.nl/apps/geoservices/geodata/dmc/kust_2013_5/kust_2013_5.zip
+wget http://www.rijkswaterstaat.nl/apps/geoservices/geodata/dmc/kust_2014_2/kust_2014_2.zip
+wget http://www.rijkswaterstaat.nl/apps/geoservices/geodata/dmc/kust_2015_2/kust_2015_2.zip
+
+unzip kust2_2011_2.zip
+unzip kust2_2012_2.zip
+unzip kust2_2013_2.zip
+unzip kust2_2014_2.zip
+unzip kust2_2015_2.zip
+unzip kust_2011_5.zip
+unzip kust_2012_5.zip
+unzip kust_2013_5.zip
+unzip kust_2014_2.zip
+unzip kust_2015_2.zip
