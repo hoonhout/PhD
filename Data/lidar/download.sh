51186faaca2fdf3373b36e5192526eab8c96d1f6
Bas Hoonhout <bas@hoonhout.com>
2016-11-03 15:28:14 +0100

    Update

--- Data/lidar/download.sh
+++ Data/lidar/download.sh
@@ -1,23 +1,26 @@
 #!/bin/bash
 
-wget http://www.rijkswaterstaat.nl/apps/geoservices/geodata/dmc/kust2_2011_2/kust2_2011_2.zip
-wget http://www.rijkswaterstaat.nl/apps/geoservices/geodata/dmc/kust2_2012_2/kust2_2012_2.zip
-wget http://www.rijkswaterstaat.nl/apps/geoservices/geodata/dmc/kust2_2013_2/kust2_2013_2.zip
-wget http://www.rijkswaterstaat.nl/apps/geoservices/geodata/dmc/kust2_2014_2/kust2_2014_2.zip
-wget http://www.rijkswaterstaat.nl/apps/geoservices/geodata/dmc/kust2_2015_2/kust2_2015_2.zip
-wget http://www.rijkswaterstaat.nl/apps/geoservices/geodata/dmc/kust_2011_5/kust_2011_5.zip
-wget http://www.rijkswaterstaat.nl/apps/geoservices/geodata/dmc/kust_2012_5/kust_2012_5.zip
-wget http://www.rijkswaterstaat.nl/apps/geoservices/geodata/dmc/kust_2013_5/kust_2013_5.zip
-wget http://www.rijkswaterstaat.nl/apps/geoservices/geodata/dmc/kust_2014_2/kust_2014_2.zip
-wget http://www.rijkswaterstaat.nl/apps/geoservices/geodata/dmc/kust_2015_2/kust_2015_2.zip
+ROOT="http://www.rijkswaterstaat.nl/apps/geoservices/geodata/dmc"
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-unzip kust2_2011_2.zip
-unzip kust2_2012_2.zip
-unzip kust2_2013_2.zip
-unzip kust2_2014_2.zip
-unzip kust2_2015_2.zip
-unzip kust_2011_5.zip
-unzip kust_2012_5.zip
-unzip kust_2013_5.zip
-unzip kust_2014_2.zip
-unzip kust_2015_2.zip
+wget -P $DIR $ROOT/kust2_2011_2/kust2_2011_2.zip
+wget -P $DIR $ROOT/kust2_2012_2/kust2_2012_2.zip
+wget -P $DIR $ROOT/kust2_2013_2/kust2_2013_2.zip
+wget -P $DIR $ROOT/kust2_2014_2/kust2_2014_2.zip
+wget -P $DIR $ROOT/kust2_2015_2/kust2_2015_2.zip
+wget -P $DIR $ROOT/kust_2011_5/kust_2011_5.zip
+wget -P $DIR $ROOT/kust_2012_5/kust_2012_5.zip
+wget -P $DIR $ROOT/kust_2013_5/kust_2013_5.zip
+wget -P $DIR $ROOT/kust_2014_2/kust_2014_2.zip
+wget -P $DIR $ROOT/kust_2015_2/kust_2015_2.zip
+
+unzip -d $DIR $DIR/kust2_2011_2.zip
+unzip -d $DIR $DIR/kust2_2012_2.zip
+unzip -d $DIR $DIR/kust2_2013_2.zip
+unzip -d $DIR $DIR/kust2_2014_2.zip
+unzip -d $DIR $DIR/kust2_2015_2.zip
+unzip -d $DIR $DIR/kust_2011_5.zip
+unzip -d $DIR $DIR/kust_2012_5.zip
+unzip -d $DIR $DIR/kust_2013_5.zip
+unzip -d $DIR $DIR/kust_2014_2.zip
+unzip -d $DIR $DIR/kust_2015_2.zip
